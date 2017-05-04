28887c4fd64f8e6910af869bc5587498bec5733e
DestinyLuong <destinyul2010@gmail.com>
2017-05-04 16:38:09 -0700

    Update guts.js

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -35,3 +35,84 @@ function logBase10(x) {
 function generateRandomNumber() {
 
 }
+function divide(x,y) {
+	return x / y;
+}
+
+console.assert(divide(1,1) === 1, "1/1 should equal 1");
+
+
+// 2. multiply
+// write a function named
+// "multiply" that takes in two
+// parameters and returns
+// their product
+
+function multiply(x,y) {
+    return x * y;    
+}
+
+console.assert(multiply (1,1) === 1, "1*1 should equal 1");
+
+// 3. subtract 
+//    write a function named
+//    "subtract" that takes in two
+// parameters and returns
+// their difference
+
+function subtract(x,y) {
+    return x - y;
+}
+
+console.assert(subtract (1,1) === 0, "1-1 should equal 0");
+
+// 4. add
+//    write a function named "add" that takes in two parameters and returns their sum
+
+function add(x,y) {
+    return x + y;   
+}
+
+console.assert(add (1,1) === 2, "1+1 should equal 2");
+
+// 5. power
+//    write a function named "power" that takes in two parameters and returns their result
+
+function power(x,y) {
+    return x ^ y;    
+}
+
+console.assert(power (1,1) === 1, "1^1 should equal 1");
+
+// 6. square_root
+//    write a function named "square_root" that takes in one parameter and returns their result
+
+function square_root(x) {
+    return x * x;
+}
+
+console.assert(square_root (1,1) === 1, "1*1 should equal 1");
+
+// 7. logBase10
+//    write a function named "logBase10" that takes in one parameter and returns their result
+
+function logBase10 (x) {
+    return x;
+}
+
+console.assert(logBase10 (1) === 1, "1 should equal 1");
+
+// 8. generateRandomNumber
+//    write a function named "generateRandomNumber" that takes in no parameters and returns a random number
+ 
+function generateRandomNumber() {
+    return Math.log;
+}
+
+console.assert(generateRandomNumber () === , "Blank should equal random");
+
+// BONUS: write assertions to test whether your functions work!
+// Here are some examples for divide(x,y), can you write more for the other functions?
+//
+// console.assert(divide(1,1) === 1, "1/1 should equal 1");
+// console.assert(divide(1,2) === 0.5, "1/2 should equal 0.5");
